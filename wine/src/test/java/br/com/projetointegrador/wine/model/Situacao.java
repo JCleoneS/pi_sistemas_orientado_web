13062291e4312c23ad10be5ff9ba7f394d0c1210
Cleone <jose_cleone@hotmail.com>
2023-03-11 17:18:49 -0300

    Tela de login

---
+++ wine/src/test/java/br/com/projetointegrador/wine/model/Situacao.java
@@ -0,0 +1,5 @@
+package br.com.projetointegrador.wine.model;
+
+public enum Situacao {
+    ATIVO, INATIVO;
+}
---
+++ wine/src/test/java/br/com/projetointegrador/wine/model/Usuario.java
@@ -0,0 +1,82 @@
+package br.com.projetointegrador.wine.model;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String cpf;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+    @Enumerated(EnumType.STRING)
+    private Grupo cargo;
+    @Enumerated(EnumType.STRING)
+    private Situacao situacao;
+
+    public Usuario() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Grupo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Grupo cargo) {
+        this.cargo = cargo;
+    }
+
+    public Situacao getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(Situacao situacao) {
+        this.situacao = situacao;
+    }
+}
