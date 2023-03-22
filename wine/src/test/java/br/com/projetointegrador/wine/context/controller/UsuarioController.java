d9d33ffa3c665fdc9873c9209698cf8670dfa42a
Will <will_andrada@hotmail.com>
2023-03-22 19:28:46 -0300

    Criação da tela de edição
    Criação da tela de edição
    
    Porém ainda estou encontrando erros

--- wine/src/test/java/br/com/projetointegrador/wine/context/controller/UsuarioController.java
+++ wine/src/test/java/br/com/projetointegrador/wine/context/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -83,6 +84,13 @@ public class UsuarioController {
     public ModelAndView update(Usuario requisicao){
         ModelAndView mv = new ModelAndView("/usuarios");
         System.out.println("Teste 'PutMapping' em '(\"usuarios/:id\")'");
+        return mv;
+    }
+
+    @GetMapping("/usuarios/{id}/edit")
+    public ModelAndView edit(@PathVariable Long id, RequisicaoNovoUsuarioDTO requisicao){
+        ModelAndView mv = new ModelAndView("usuarios/edit");
+
         return mv;
     }
 }
