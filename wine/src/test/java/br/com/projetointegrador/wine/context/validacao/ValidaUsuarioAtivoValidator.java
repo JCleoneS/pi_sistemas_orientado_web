5463780dcc8be511e70bbaf36d3f8a63d670ad60
Matheus Castro <matheusaccastro@gmail.com>
2023-05-03 23:42:06 -0300

    Incluindo lógica de inclusão de imagens.

--- wine/src/test/java/br/com/projetointegrador/wine/context/validacao/ValidaUsuarioAtivoValidator.java
+++ wine/src/test/java/br/com/projetointegrador/wine/context/validacao/ValidaUsuarioAtivoValidator.java
@@ -1,10 +1,13 @@
 package br.com.projetointegrador.wine.context.validacao;
 
 import br.com.projetointegrador.wine.context.model.Situacao;
+import br.com.projetointegrador.wine.context.model.Usuario;
 import br.com.projetointegrador.wine.context.repository.UsuarioRepository;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class ValidaUsuarioAtivoValidator implements ConstraintValidator<ValidaUsuarioAtivo, String> {
 
@@ -12,6 +15,11 @@ public class ValidaUsuarioAtivoValidator implements ConstraintValidator<ValidaUs
     private UsuarioRepository usuarioRepository;
 
     public boolean isValid(String email, ConstraintValidatorContext context) {
-        return usuarioRepository.findByEmail(email).get(0).getSituacao().equals(Situacao.ATIVO);
+        List<Usuario> listaUsuario = usuarioRepository.findByEmail(email);
+        boolean result = false;
+        if(listaUsuario != null && listaUsuario.size() > 0) {
+            result = listaUsuario.get(0).getSituacao().equals(Situacao.ATIVO);
+        }
+        return result;
     }
 }
